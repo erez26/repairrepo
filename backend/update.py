53d08dacd1e631c8e36488304f18e878ff1c685c
erez26 <46246496+erez26@users.noreply.github.com>
2019-01-04 08:11:43 +0300

    Add files via upload

---
+++ backend/update.py
@@ -0,0 +1,53 @@
+#Push data from file to pymongo db
+#Author: Yashar Asgari
+import csv, json, pymongo, argparse
+from pymongo import MongoClient
+
+
+parser = argparse.ArgumentParser(description = 'argparse for organism type')
+parser.add_argument('organism')
+args = parser.parse_args()
+
+
+txtfile = open('testUpsert.txt', 'r') #txt file with subset of sample data
+
+reader = csv.DictReader(txtfile, delimiter = '\t')
+
+
+connection = MongoClient('mongodb://yasgari:701415@sancarlabdb-shard-00-00-v0rht.mongodb.net:27017,sancarlabdb-shard-00-01-v0rht.mongodb.net:27017,sancarlabdb-shard-00-02-v0rht.mongodb.net:27017/data?ssl=true&replicaSet=SancarLabDB-shard-0&authSource=admin')
+db = connection.data
+collection = db['gene']
+    
+for row in reader:
+
+    data = dict(row) #convert from OrderedDict to dict object
+    data['organism'] = 'mouse'
+    geneName = data['name']
+
+
+    cursor = collection.find({'$and':[{"organism":args.organism}, {"name": geneName}]})
+
+    if(cursor.count() == 0): #insert entry if doesn't exist in db
+        
+        iterData = dict.copy(data)
+        for key in iterData.keys():
+            if(iterData[key] is None):
+                data.pop(key,None)
+
+        collection.insert(data)
+    elif(cursor.count() > 1): #there should only be 1 document for each organism/gene pair
+        raise ValueError("Duplicate document found. More than one found for Gene " + geneName + " for " + args.organism)
+
+    for oldDoc in cursor: 
+            for key in data.keys():
+        
+                oldDoc[key] = data[key]
+                uqId = oldDoc['_id']
+                if(data[key] is None):
+                    oldDoc.pop(key, None) #get rid of null fields
+                else:
+                    #update using document's unique ID
+                    collection.update({"_id": uqId}, {"$set":{key:oldDoc[key]}})    
+
+
+connection.close()
---
+++ frontend/browserScript.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+
+
+    $('#header').load('header.html', getCurrentPage());
+    $('#footer').load('footer.html');
+
+    const organism = getCurrentOrganism();
+    console.log('current organism: ' + organism)
+
+    //CODE FOR IGV.JS GENOME BROSWER 
+
+    var igvDiv =  document.getElementById("igvContainer");
+    var options = {
+        genome: "hg19",
+        locus: "chr8:128,747,267-128,754,546",
+        tracks: [
+            {
+                type: 'alignment',
+                format: 'bam',
+                url: 'https://data.broadinstitute.org/igvdata/1KG/b37/data/HG02450/alignment/HG02450.mapped.ILLUMINA.bwa.ACB.low_coverage.20120522.bam',
+                name: 'HG02450'
+            }
+        ]
+    };
+    igv.createBrowser(igvDiv, options)
+    .then(function (browser) {
+        console.log("Created IGV browser");
+    })
+
+
+    //helper functions
+
+    function getCurrentPage(){
+        var count = 0;
+        console.log('count : ', $('.navLinks').length)
+        $(".navLinks").each(function() {
+            count+=1
+            console.log('this.href' + this.href)
+            console.log('windowloc' + window.location.href)
+            if (this.href == window.location.href) {
+                $(this).addClass("active");
+            }
+        });
+        console.log('# of hrefs' + count)
+    }
+
+    function getCurrentOrganism(){
+        var url = window.location.href
+        var organism = url.split("/").pop().split("browser")
+        return organism.slice(0,-1);
+    }
+
+});
